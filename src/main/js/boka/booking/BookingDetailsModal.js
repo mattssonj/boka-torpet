fb721bc6186a0dccdee18430d18b5a06a9e230dc
Joakim Mattsson <joamat91@gmail.com>
2021-05-15 09:50:34 +0200

    Show more info button which shows bookers contact info on booking

---
+++ src/main/js/boka/booking/BookingDetailsModal.js
@@ -0,0 +1,63 @@
+import React, {useEffect, useState} from 'react';
+import Axios from "axios";
+
+import {Button, Form, Modal} from "react-bootstrap";
+import {error} from "../../Toaster";
+
+const initialInformation = {
+    name: '',
+    phone: '',
+    email: '',
+}
+
+export default function BookingDetailsModal({show, hideFunction, booking}) {
+
+    const [userInformation, setUserInformation] = useState(initialInformation)
+
+    useEffect(() => {
+        if (show) getInformation()
+    }, [show])
+
+    const getInformation = () => {
+        Axios.get('/api/users/' + booking.booker)
+            .then(response => {
+                setUserInformation(response.data)
+            }).catch(response => {
+            console.log(response.response)
+            error('Fel när kontaktuppgifter skulle hämtas: ' + response.response.data.message)
+        })
+    }
+
+    return (
+        <Modal show={show} onHide={hideFunction}>
+            <Modal.Header><Modal.Title>Kontaktuppgifter</Modal.Title></Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Form.Text className="text-muted">
+                    </Form.Text>
+                    <Form.Group controlId="name">
+                        <Form.Label>Namn</Form.Label>
+                        <Form.Control type="text"
+                                      onChange={e => setUserInformation({...userInformation, name: e.target.value})}
+                                      value={userInformation.name} disabled/>
+                    </Form.Group>
+                    <Form.Group controlId="phone">
+                        <Form.Label>Telefon</Form.Label>
+                        <Form.Control type="text"
+                                      onChange={e => setUserInformation({...userInformation, phone: e.target.value})}
+                                      value={userInformation.phone} disabled/>
+                    </Form.Group>
+                    <Form.Group controlId="email">
+                        <Form.Label>E-post</Form.Label>
+                        <Form.Control type="email"
+                                      onChange={e => setUserInformation({...userInformation, email: e.target.value})}
+                                      value={userInformation.email} disabled/>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={hideFunction}>Stäng</Button>
+            </Modal.Footer>
+        </Modal>
+    );
+}
