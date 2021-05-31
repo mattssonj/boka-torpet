63a0b21dad3d3fa0617e83a5020435a8cc87bf96
Joakim Mattsson <joamat91@gmail.com>
2021-05-31 12:41:33 +0200

    Fixing bug with update button

--- src/main/js/booking/BookingListComponent.js
+++ src/main/js/booking/BookingListComponent.js
@@ -4,46 +4,54 @@ import Axios from "axios";
 import BookingListRow from "./BookingListElement";
 import {Button, Col, Row} from "react-bootstrap";
 
-export default function Bookings() {
+const initialShowing = {
+    ongoing: [],
+    upcoming: []
+}
 
-    const [bookings, setBookings] = useState([])
-    const [showing, setShowing] = useState([])
+const getBookings = (responseHandler) => {
+    Axios.all([
+        Axios.get('/api/bookings'),
+        Axios.get('/api/bookings')
+    ]).then(Axios.spread((ongoing, upcoming) => {
+        responseHandler({ongoing: [], upcoming: upcoming.data})
+    })).catch(error => {
+        console.log('Error occurred when getting bookings: ' + error.response)
+        responseHandler(initialShowing)
+    })
+}
+
+export default function Bookings() {
+    const [bookings, setBookings] = useState(initialShowing)
+    const [showing, setShowing] = useState(initialShowing)
     const [showAll, setShowAll] = useState(true)
-    const [loggedInUsername, setLoggedInUsername] = useState('')
+    const [loggedInUsername, setLoggedInUsername] = useState(null)
 
     useEffect(() => {
-        getBookings(response => {
-            setBookings(response)
-            setShowing(response)
+        getBookings(bookings => {
+            setBookings(bookings)
+            setShowing(bookings)
         })
         getLoggedInUsername()
-    }, []); // The empty array here makes the hook only trigger once
-
+    }, [])
 
     const updateBookings = () => {
-        getBookings(response => {
-            setBookings(response)
-            showAll ? showAllBookings() : showLoggedInBookings()
+        getBookings(bookings => {
+            showAll ? showAllBookings(bookings) : showLoggedInBookings(bookings)
+            setBookings(bookings)
         })
-    };
-
-    const getBookings = (responseHandler) => {
-        Axios.get('/api/bookings').then(response => {
-            responseHandler(response.data)
-        }).catch(error => {
-            console.log('Unable to receive bookings. Error {}', error)
-            responseHandler([])
-        });
     }
 
-    const showAllBookings = async () => {
+    const showAllBookings = (newBookings) => {
+        newBookings ? setShowing(newBookings) : setShowing(bookings)
         setShowAll(true)
-        setShowing(bookings)
-    };
+    }
 
-    const showLoggedInBookings = async () => {
+    const showLoggedInBookings = (newBookings) => {
+        const bookingsToShow = newBookings ? newBookings : bookings
+        const bookerFilter = (bookings) => bookings.filter(booking => booking.booker === loggedInUsername)
+        setShowing({ongoing: bookerFilter(bookingsToShow.ongoing), upcoming: bookerFilter(bookingsToShow.upcoming)})
         setShowAll(false)
-        setShowing(bookings.filter(booking => booking.booker === loggedInUsername))
     }
 
     const getLoggedInUsername = () => {
@@ -55,25 +63,36 @@ export default function Bookings() {
         })
     }
 
+    const mapListItems = (list, message) => {
+        const listItems = list.map(booking => (
+            <div key={booking.id}>
+                <BookingListRow booking={booking} username={loggedInUsername}/>
+                <br/>
+            </div>
+        ))
+        return listItems.length > 0 ? listItems : message
+    }
+
     return (
         <Col>
             <br/>
             <Row className="justify-content-md-center">
                 <Col md="auto"><Button variant="outline-secondary" onClick={updateBookings}>Uppdatera listan</Button></Col>
-                <Col md="auto"><Button variant={!showAll ? "secondary" : "outline-secondary"} onClick={showLoggedInBookings}>Visa
+                <Col md="auto"><Button variant={!showAll ? "secondary" : "outline-secondary"} onClick={() => showLoggedInBookings(null)}>Visa
                     mina</Button></Col>
-                <Col md="auto"><Button variant={showAll ? "secondary" : "outline-secondary"} onClick={showAllBookings}>Visa
+                <Col md="auto"><Button variant={showAll ? "secondary" : "outline-secondary"} onClick={() => showAllBookings(null)}>Visa
                     alla</Button></Col>
             </Row>
+            <Row><Col><h1>På torpet just nu</h1></Col></Row>
             <Row>
                 <Col>
-                    <br/>
-                    {showing.map(booking => (
-                        <div key={booking.id}>
-                            <BookingListRow booking={booking} username={loggedInUsername}/>
-                            <br/>
-                        </div>
-                    ))}
+                    {mapListItems(showing.ongoing, 'Ingen verkar vara där just nu')}
+                </Col>
+            </Row>
+            <Row><Col><h1>Kommande</h1></Col></Row>
+            <Row>
+                <Col>
+                    {mapListItems(showing.upcoming, 'Igen har bokat att de ska åka upp')}
                 </Col>
             </Row>
         </Col>
