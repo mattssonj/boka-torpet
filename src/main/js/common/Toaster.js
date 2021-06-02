a0a353bed8d8747051298d2539b9e51568b7454c
Joakim Mattsson <joamat91@gmail.com>
2021-06-02 20:19:24 +0200

    update readme and move files and documentation

--- src/main/js/common/Toaster.js
+++ src/main/js/common/Toaster.js
@@ -2,19 +2,15 @@ import {toast} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 // This overrides the background-colors to match Bootstrap 4
-import "../ToasterStyle.css"
+import "./ToasterStyle.css"
+
+// https://github.com/fkhadra/react-toastify#readme for more information
 
 const toaster = {
 
-    success: function (message) {
-        toast.success(message)
-    },
-    error: function (message) {
-        toast.error(message)
-    },
-    warning: function (message) {
-        toast.warn(message)
-    }
+    success: function (message) {toast.success(message)},
+    error: function (message) {toast.error(message)},
+    warning: function (message) {toast.warn(message)}
 
 }
 
