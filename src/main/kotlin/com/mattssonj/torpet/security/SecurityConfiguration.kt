5fbf2195784bb09dadab0ecbb33ccf9538cbd6b2
Joakim Mattsson <joamat91@gmail.com>
2021-06-09 20:46:07 +0200

    Update security config to always redirect to https

--- src/main/kotlin/com/mattssonj/torpet/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/mattssonj/torpet/security/SecurityConfiguration.kt
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -27,6 +28,8 @@ class SecurityConfiguration(private val datasource: DataSource) : WebSecurityCon
 
     override fun configure(http: HttpSecurity) {
         http
+            .requiresChannel().anyRequest().requiresSecure()
+            .and()
             .authorizeRequests()
             .mvcMatchers("/admin").hasAnyRole(Roles.ADMIN, Roles.DEV)
             .anyRequest().authenticated()
