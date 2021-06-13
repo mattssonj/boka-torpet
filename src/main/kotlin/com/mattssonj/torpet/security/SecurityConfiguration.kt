0512cfefc3a655870bf684818a7fb394559e66f3
Joakim Mattsson <joamat91@gmail.com>
2021-06-13 21:04:25 +0200

    Testing https

--- src/main/kotlin/com/mattssonj/torpet/security/SecurityConfiguration.kt
+++ src/main/kotlin/com/mattssonj/torpet/security/SecurityConfiguration.kt
@@ -6,10 +6,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.provisioning.JdbcUserDetailsManager
 import org.springframework.security.provisioning.UserDetailsManager
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+import org.springframework.security.web.server.SecurityWebFilterChain
 import javax.sql.DataSource
 
 object Roles {
@@ -27,6 +29,8 @@ class SecurityConfiguration(private val datasource: DataSource) : WebSecurityCon
 
     override fun configure(http: HttpSecurity) {
         http
+            .requiresChannel().anyRequest().requiresSecure()
+            .and()
             .authorizeRequests()
             .mvcMatchers("/admin").hasAnyRole(Roles.ADMIN, Roles.DEV)
             .anyRequest().authenticated()
